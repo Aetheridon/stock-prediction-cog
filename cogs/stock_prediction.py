3bc96387735f6fd9d3ff462e9e4804830160d057
Aetheridon <lewisvr@protonmail.com>
2024-02-17 13:26:42 +0000

    Added arguments for the predict command, aswell as a help command, improved logging to be added.

--- cogs/stock_prediction.py
+++ cogs/stock_prediction.py
@@ -6,8 +6,15 @@ class StockPrediction(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def predict(self, ctx):
-        await ctx.send("predicting")
+    async def predict(self, ctx, *args):
+        if args:
+            await ctx.send(f"fetching data for stock {args[0]}")
+        else:
+            await ctx.send("no arguments provided! use !predict_help for help related to the predict command...")
+
+    @commands.command()
+    async def predict_help(self, ctx):
+        await ctx.send("to use the prediction feature, simply type !predict followed by a stock ticker, i.e for stock information related to tesla, !predict tsla")
 
 async def setup(client):
     await client.add_cog(StockPrediction(client=client))
